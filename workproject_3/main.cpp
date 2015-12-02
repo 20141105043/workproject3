59bea4bf5b6a68f2bae4f10cdc693d8b8b8010c4
20141105043 <670140530@qq.com>
2015-12-02 21:12:20 +0800

    Linked List
    建立一个链表文件，实现学生信息管理系统（包括姓名和年龄），循环输入，循环输出。

--- workproject_3/main.cpp
+++ workproject_3/main.cpp
@@ -10,6 +10,29 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    struct Student
+    {
+        char name[10];
+        int age;
+        Student *next;
+    };
+    Student *p,*q,*head;
+    head=new Student;
+    head->next=0;
+    p=head;
+    for(int i=0;i<4;i++)
+    {
+        q=new Student;
+        p->next=q;
+        q->next=0;
+        scanf("%s %d",q->name,&(q->age));
+        p=q;
+    }
+    p=head->next;
+    while(p!=0)
+    {
+        printf("%s  %d p=%x\n",p->name,p->age,p);
+        p=p->next;
+    }
     return 0;
 }
